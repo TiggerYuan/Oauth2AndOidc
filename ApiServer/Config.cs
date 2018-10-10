8601c92f153cd4c521178b92a540fbb4c38a0577
Tigger <775142365@qq.com>
2018-10-10 16:11:06 +0800

    添加项目文件。

---
+++ ApiServer/Config.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ApiServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client{
+                    ClientId="api",
+                    AllowedGrantTypes=GrantTypes.Implicit,
+                    ClientSecrets={
+                        new Secret("secret", HashExtensions.Sha256("secret"))
+                    },
+                    AllowedCorsOrigins={ "api1"}
+                }
+            };
+        }
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource()
+            };
+        }
+    }
+}
---
+++ MvcServer/Config.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MvcServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client{
+                    ClientId="mvc",
+                    AllowedGrantTypes=GrantTypes.Implicit,
+                    ClientSecrets={
+                        new Secret("secret", HashExtensions.Sha256("secret"))
+                    },
+                    AllowedCorsOrigins={ "api1"}
+                }
+            };
+        }
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("api1")
+            };
+        }
+    }
+}
